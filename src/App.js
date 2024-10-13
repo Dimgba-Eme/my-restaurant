479aaf4ff33be4c5124528e3869f6796107869a9
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-13 12:15:35 +0100

    Added site pages and imported all inside App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Reservations from './pages/Reservations';
+import OrderOnline from './pages/OrderOnline';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      Home
+      <Home />
+      <About />
+      <Menu />
+      <Reservations />
+      <OrderOnline />
+      <Login />
     </div>
   );
 }
