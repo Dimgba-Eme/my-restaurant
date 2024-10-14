99a318c4197c9de17a500fc25310ba2d5aaa9171
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-14 12:49:54 +0100

    Updated Header and NavBar components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
-import About from './pages/About';
-import Menu from './pages/Menu';
-import Reservations from './pages/Reservations';
-import OrderOnline from './pages/OrderOnline';
-import Login from './pages/Login';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Home />
-      <About />
-      <Menu />
-      <Reservations />
-      <OrderOnline />
-      <Login />
-    </div>
+      
+    </>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function Header() {
   return (
     <>
      <header className='page-header'>
-       
+        <h1>Hello World</h1>
+        <NavBar />
     </header> 
     </>
   )
