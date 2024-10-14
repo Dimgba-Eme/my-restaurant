c141f294076478364f38766b99e2d09661cac977
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-14 13:36:35 +0100

    Styled and aligned header contents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
 function NavBar() {
-  return (
-    <>
-     <nav className='navBar'>
-        <ul className='list-container'>
-            <li><a href='/'>Home</a></li>
-            <li><a href='/about'>About</a></li>
-            <li><a href='/menu'>Menu</a></li>
-            <li><a href='/reservations'>Reservations</a></li>
-            <li><a href='/order-online'>Order online</a></li>
-            <li><a href='/login'>Login</a></li>
-        </ul>
-    </nav> 
-    </>
-  )
+    return (
+        <>
+            <nav className='navBar'>
+                <ul className='list-container'>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/menu'>Menu</a></li>
+                    <li><a href='/reservations'>Reservations</a></li>
+                    <li><a href='/order-online'>Order online</a></li>
+                    <li><a href='/login'>Login</a></li>
+                </ul>
+            </nav>
+        </>
+    )
 }
 
 export default NavBar
