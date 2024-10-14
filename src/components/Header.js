34cc96f454b7c732bac8b29088963df5c8143392
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-14 16:45:39 +0100

    Made the page header responsive on all devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import NavBar from './NavBar';
 import Logo from '../assets/images/lemon logo.jpg';
+import Menu from '../assets/images/🦆 icon _hamburger menu.svg'
+import Basket from '../assets/images/basket .svg'
 
 function Header() {
   return (
     <>
      <header className='page-header'>
+      <div className='logo-container'>
+      <img src={Menu} className='hamburger-menu' alt='hamburger menu' />
         <img src={Logo} className='logo' alt='brand logo' />
+        <img src={Basket} className='basket' alt='basket' />
+      </div>
+
         <NavBar />
     </header> 
     </>
