7f5c453aebdd2661806d610024d76414fe9d82f7
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-16 15:58:15 +0100

    Added date and time to the Booking Form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,7 +3,19 @@ import React from 'react'
 function BookingForm() {
   return (
     <>
-      <form></form>
+      <form>
+          <label htmlFor='res-date'>Choose a date</label>
+          <input type='date' id='res-date' />
+          <label>Choose time</label>
+          <select>
+            <option>17:00</option>
+            <option>18:00</option>
+            <option>19:00</option>
+            <option>20:00</option>
+            <option>21:00</option>
+            <option>22:00</option>
+          </select>
+      </form>
     </>
   )
 }
