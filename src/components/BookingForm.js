6b2fa4fc98df13de91fa97741bc903fa8b35f36c
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-16 16:30:06 +0100

    Added Occasion and Request button

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,9 +3,13 @@ import React from 'react'
 function BookingForm() {
   return (
     <>
-      <form>
+      <form className='res-form'>
+        <div className='reservation'>
           <label htmlFor='res-date'>Choose a date</label>
           <input type='date' id='res-date' />
+        </div>
+
+         <div className='reservation'>
           <label htmlFor='res-time'>Choose time</label>
           <select id='res-time'>
             <option>17:00</option>
@@ -15,11 +19,26 @@ function BookingForm() {
             <option>21:00</option>
             <option>22:00</option>
           </select>
+         </div>
 
+          <div className='reservation'>
           <label htmlFor='guests'>Number of guests</label>
           <input type='number' id='guests' placeholder='1' min='1' max='10' />
+          </div>
 
-          
+          <div className='reservation'>
+          <label htmlFor='occasion'>Occasion</label>
+          <select id='occasion'>
+            <option>Birthday</option>
+            <option>Anniversary</option>
+            <option></option>
+          </select>
+          </div>
+
+         <div className='reservation'>
+          <input type='submit' value='Make your reservation' />
+         </div>
+
       </form>
     </>
   )
