aad8ae62d496a1fd245dd9472d4b9279658a3204
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-29 16:23:37 +0100

    Made the hamburger menu responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
-import React from 'react'
 import NavBar from './NavBar';
 import Logo from '../assets/images/lemon logo.jpg';
-import Menu from '../assets/images/🦆 icon _hamburger menu.svg'
 import Basket from '../assets/images/basket .svg'
 
 function Header() {
@@ -9,7 +7,6 @@ function Header() {
     <>
       <header className='page-header'>
         <div className='logo-container'>
-          <img src={Menu} className='hamburger-menu' alt='hamburger menu' />
           <img src={Logo} className='logo' alt='brand logo' />
           <img src={Basket} className='basket' alt='basket' />
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+import Menu from '../assets/images/🦆 icon _hamburger menu.svg'
+
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <>
-            <nav className='navBar'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
-                <NavLink to='/menu'>Menu</NavLink>
-                <NavLink to='/reservations'>Reservations</NavLink>
-                <NavLink to='/order-online'>Order online</NavLink>
-                <NavLink to='/login'>Login</NavLink>
+            <img src={Menu} className='hamburger-menu' alt='hamburger menu' onClick={() => setIsOpen(!isOpen)} />
+
+
+            <nav className={isOpen ? "open" : ""}>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/about'>About</NavLink>
+                    <NavLink to='/menu'>Menu</NavLink>
+                    <NavLink to='/reservations'>Reservations</NavLink>
+                    <NavLink to='/order-online'>Order online</NavLink>
+                    <NavLink to='/login'>Login</NavLink>
             </nav>
+
 
         </>
     )
