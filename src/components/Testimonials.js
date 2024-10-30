3d8700622ed456323cad73d51cc9a5199e0ccf6c
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-30 11:34:51 +0100

    Added three users in the testimonial section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,42 @@
 import React from 'react'
 
+import User1 from '../assets/images/user-1.jpg'
+import User2 from '../assets/images/user-2.jpg'
+import User3 from '../assets/images/user-3.jpg'
+
 function Testimonials() {
-  return (
-    <>
-     <h1>Testimonials</h1> 
-    </>
-  )
+    return (
+        <>
+            <section>
+                <h1>Testimonials</h1>
+                <div>
+
+                    <div>
+                        <img src={User1} alt='user-1' />
+                        <h2>John Doe</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Excepturi tenetur obcaec,
+                            praesentiumcumque, autem similique de.</p>
+                    </div>
+                    <div>
+                        <img src={User2} alt='user-2' />
+                        <h2>Laura Smith</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Excepturi tenetur obcaec,
+                            praesentiumcumque, autem similique de.</p>
+                    </div>
+                    <div>
+                        <img src={User3} alt='user-3' />
+                        <h2>Annabel Donald</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Excepturi tenetur obcaec,
+                            praesentiumcumque, autem similique de.</p>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Testimonials
