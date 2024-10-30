3caee1a4f4a7295cf5a47f31d21c813de55196fa
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-10-30 13:27:48 +0100

    Created Owners Component

---
+++ src/components/Owners.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Owners() {
+    return (
+        <>
+            <section className='owners-container'>
+                <div className='owners-description'>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur
+                        adipisicing elit. Excepturi tenetur obcaec,
+                        praesentiumcumque, autem similique de.
+                        autem similique deleniti a magnam commodi,
+                        fugit explicabo aspernatur qui sequi sint consecteturnam expedita architecto consequatur.
+                    </p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Owners
