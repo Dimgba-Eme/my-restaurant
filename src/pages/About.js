23e5129a6adb4daa7ee75e46e72e4f0481713bf4
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-11-02 13:19:06 +0100

    Created AboutMain component for the About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header';
+import AboutMain from '../components/AboutMain';
+import Footer from '../components/Footer'
 
 function About() {
   return (
     <>
      <Header /> 
+     <AboutMain />
+     <Footer />
     </>
   )
 }
