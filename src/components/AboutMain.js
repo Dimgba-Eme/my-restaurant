b3eb94b46cb14f0111151727f2c9cac833a2eb1a
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-11-02 13:55:14 +0100

    Added images in the about page through the AboutMain component

--- src/components/AboutMain.js
+++ src/components/AboutMain.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
+import PIC1 from '../assets/images/Mario and Adrian A.jpg'
+import PIC2 from '../assets/images/Mario and Adrian b.jpg'
+import PIC3 from '../assets/images/restaurant chef B.jpg'
+import PIC4 from '../assets/images/restaurant.jpg'
+
 function AboutMain() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <section className='about-wrapper'>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur
+                    adipisicing elit. Excepturi tenetur obcaec,
+                    praesentiumcumque, autem similique de.
+                    autem similique deleniti a magnam commodi,
+                    fugit explicabo aspernatur qui sequi sint consecteturnam expedita architecto consequatur.
+                </p>
+
+                <div className='about-pics'>
+                    <img src={PIC1} alt='first pic' />
+                    <img src={PIC2} alt='second pic' />
+                    <img src={PIC3} alt='third pic' />
+                    <img src={PIC4} alt='fourth pic' />
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default AboutMain
