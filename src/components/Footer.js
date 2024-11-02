def41962033f6927ef41a2d345d265815b0fc3a4
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-11-02 11:43:14 +0100

    Styled and aligned the contents in the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 import FooterLogo from '../assets/images/footer_image.png'
 
+import FB from '../assets/images/icon-facebook.svg'
+import INSTA from '../assets/images/icon-instagram.svg'
+import TUBE from '../assets/images/icon-youtube.svg'
+import { useNavigate } from 'react-router-dom'
+
 function Footer() {
+    const navigate = useNavigate();
     return (
         <>
             <footer className='footer-wrapper'>
@@ -11,29 +17,31 @@ function Footer() {
                     <img src={FooterLogo} alt='footer_logo' />
 
                     <div className='navigation'>
-                        <h2>Doormat Navigation</h2>
-                        <p>Home</p>
-                        <p>About</p>
-                        <p>Menu</p>
-                        <p>Reservations</p>
-                        <p>Order online</p>
-                        <p>Login</p>
+                        <h4>Doormat Navigation</h4>
+                        <p onClick={() => navigate("/")}>Home</p>
+                        <p onClick={() => navigate("/about")}>About</p>
+                        <p onClick={() => navigate("/menu")}>Menu</p>
+                        <p onClick={() => navigate("/reservations")}>Reservations</p>
+                        <p onClick={() => navigate("/order-online")}>Order online</p>
+                        <p onClick={() => navigate("/login")}>Login</p>
                     </div>
 
                     <div className='contact'>
-                        <h2>Contact</h2>
+                        <h4>Contact</h4>
                         <p>Address</p>
-                        <p>Phone number</p>
-                        <p>Email</p>
+                        <p>Tel: +234 801 283 6797</p>
+                        <p>Email: hello@little_lemon.com</p>
                     </div>
 
                     <div className='socials-links'>
-                        <h2>Social Media Links</h2>
-                        <p>Links</p>
+                        <h4>Social Media Links</h4>
+                        <span><img src={FB} alt='facebook icon' /></span>
+                        <span><img src={INSTA} alt='instagram icon' /></span>
+                        <span><img src={TUBE} alt='youtube icon' /></span>
                     </div>
 
                 </div>
-                <p>All Rights Reserved</p>
+                <p className='copy'>All Rights Reserved &copy; 2024</p>
             </footer>
         </>
     )
