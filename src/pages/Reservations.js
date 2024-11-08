32bb86d0eae3574fa5cbc1244dfde602db60d55f
Dimgba Eme <dimgbaagwueme@gmail.com>
2024-11-08 13:05:31 +0100

    Added the Footer component in the Reservations page

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BookingForm from '../components/BookingForm'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function Reservations() {
   return (
     <>
       <Header />
       <BookingForm />
+      <Footer />
     </>
   )
 }
